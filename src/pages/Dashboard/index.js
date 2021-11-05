4383cdda0d56e5d9c2e59eb1d5b592bd46d6133a
tiagolpires <tiagolpires@hotmail.com>
2021-11-04 23:45:11 -0300

    feat: get and display students

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,16 +1,31 @@
+import { useEffect, useState } from 'react';
 import Header from '../../components/Header'
 import Card from '../../components/Card'
 import './style.css'
+import { useQuery } from "@apollo/client"
+import { GET_STUDENTS } from '../../GraphQL/Queries'
 
-const Index = () => {
+  
+  const Index = () => {
+    const { data } = useQuery(GET_STUDENTS)
+    const [students, setStudents] = useState([])
+
+    useEffect(() => {
+      if (data) setStudents(data.students)
+    }, [data]);
+
     return (
         <>
             <Header/>
             <main className="main-content">
                 <div className="cards-container">
-                    <Card name={'Tiago Pires'} email={'tiagolpires@hotmail.com'} document={'12345678123'}/>   
-                    <Card name={'Tiago Pires'} email={'tiagolpires@hotmail.com'} document={'12345678123'}/>   
-                    <Card name={'Tiago Pires'} email={'tiagolpires@hotmail.com'} document={'12345678123'}/>   
+                    {students.length > 0 && students.map(student => (
+                        <Card name={student.name} email={student.email} document={student.document} key={student.id}/>   
+                    ))}
+                    
+                    {students.length === 0 && 
+                        <span>Nenhum estudante cadastrado</span>
+                    }
                 </div>
             </main>
         </>
